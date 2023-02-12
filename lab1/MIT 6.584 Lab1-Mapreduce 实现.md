d7634889236af7e7e2cc2bebefcbfb938d9db9b9
David <100999898+T4t4KAU@users.noreply.github.com>
2023-02-12 19:57:38 +0800

    Add files via upload
    MIT 6.584 labs

---
+++ lab1/MIT 6.584 Lab1-Mapreduce 实现.md
@@ -0,0 +1,5 @@
+# MIT 6.584 Lab1-Mapreduce 实现
+
+la1 address: https://pdos.csail.mit.edu/6.824/labs/lab-mr.html
+
+## 代码实现
---
+++ lab1/mr/rpc.go
@@ -0,0 +1,78 @@
+package mr
+
+//
+// RPC definitions.
+//
+// remember to capitalize all names.
+//
+
+import (
+	"os"
+	"strconv"
+)
+
+//
+// example to show how to declare the arguments
+// and reply for an RPC.
+//
+
+type ExampleArgs struct {
+	X int
+}
+
+type ExampleReply struct {
+	Y int
+}
+
+// Add your RPC definitions here.
+type RegistrArgs struct{}
+
+type RegisterReply struct {
+	Id      int32
+	NReduce int
+}
+
+type AssignArgs struct {
+	Id int32 // worker id
+}
+
+type AssignReply struct {
+	Id   int32 // task id
+	File string
+	Flag string
+}
+
+type RoundArgs struct {
+	WID  int32
+	TID  int32
+	Flag string
+}
+
+type RoundReply struct {
+}
+
+type FinishArgs struct {
+	WID  int32
+	TID  int32
+	Flag string
+}
+
+type FinishReply struct {
+	Accept bool
+}
+
+type ReviewArgs struct {
+	Id int32
+}
+
+type ReviewReply struct{}
+
+// Cook up a unique-ish UNIX-domain socket name
+// in /var/tmp, for the coordinator.
+// Can't use the current directory since
+// Athena AFS doesn't support UNIX-domain sockets.
+func coordinatorSock() string {
+	s := "/var/tmp/5840-mr-"
+	s += strconv.Itoa(os.Getuid())
+	return s
+}
