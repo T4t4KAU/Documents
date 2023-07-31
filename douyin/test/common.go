8440af5097d5ac30896855e9107d3464dc420951
David <100999898+T4t4KAU@users.noreply.github.com>
2023-08-01 02:22:15 +0800

    Delete douyin directory

--- douyin/test/common.go
+++
@@ -1,6 +0,0 @@
-package test
-
-const (
-	serverAddr = "http://localhost:18005"
-	userToken  = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA3MTExMzUsIm9yaWdfaWF0IjoxNjkwNjI0NzM1LCJ1c2VyX2lkIjoxMDE0fQ.9fppAfwuYIP-6cxI_n7mATMx1Bspm7Ts1ZA-gCf4LKQ"
-)
